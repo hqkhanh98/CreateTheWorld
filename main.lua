25b33de9e5f47d15bcb8e1f68f02a1b63b2c92e8
hqkhanh98 <tsuuuuuu66@gmail.com>
2019-11-08 10:12:54 +0700

    upload template

--- main.lua
+++ main.lua
@@ -5,3 +5,6 @@
 -----------------------------------------------------------------------------------------
 
 local composer = require "composer"
+local options = { time = 2000, effect = "fade" }
+
+composer.gotoScene( "scenes.intro", options )
