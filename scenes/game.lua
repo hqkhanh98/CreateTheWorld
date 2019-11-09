1d9123bdeb45feeec63b51f73085fe039c49e999
Nguyễn Nam <npnam6626@gmail.com>
2019-11-09 10:10:26 +0700

    update

--- scenes/game.lua
+++ scenes/game.lua
@@ -2,7 +2,7 @@
 local composer = require( "composer" )
 
 local scene = composer.newScene()
-
+local a
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
