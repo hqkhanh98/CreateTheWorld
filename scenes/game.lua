4b666c20c429bd35e5d854391742fdd66dfab745
hqkhanh98 <tsuuuuuu66@gmail.com>
2019-11-09 10:13:19 +0700

    upload

--- scenes/game.lua
+++ scenes/game.lua
@@ -1,15 +1,16 @@
-
 local composer = require( "composer" )
-
 local scene = composer.newScene()
+--=================================
+
+--=================================
 
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
-local background
+--=================================
 
-
+--=================================
 
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
@@ -20,9 +21,14 @@ function scene:create( event )
 
     local sceneGroup = self.view
     -- Code here runs when the scene is first created but has not yet appeared on screen
+    --=================================
+
+    --=================================
 
 end
+--=================================
 
+--=================================
 
 -- show()
 function scene:show( event )
@@ -32,10 +38,14 @@ function scene:show( event )
 
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
+        --=================================
 
+        --=================================
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
+        --=================================
 
+        --=================================
     end
 end
 
@@ -48,22 +58,32 @@ function scene:hide( event )
 
     if ( phase == "will" ) then
         -- Code here runs when the scene is on screen (but is about to go off screen)
+        --=================================
 
+        --=================================
     elseif ( phase == "did" ) then
         -- Code here runs immediately after the scene goes entirely off screen
+        --=================================
 
+        --=================================
     end
 end
+--=================================
 
+--=================================
 
 -- destroy()
 function scene:destroy( event )
 
     local sceneGroup = self.view
     -- Code here runs prior to the removal of scene's view
+    --=================================
 
+    --=================================
 end
+--=================================
 
+--=================================
 
 -- -----------------------------------------------------------------------------------
 -- Scene event function listeners
