d3ef27f0d4f81727a7e7966075730338fedccc26
hqkhanh98 <tsuuuuuu66@gmail.com>
2019-11-09 22:45:20 +0700

    upload

--- scenes/game.lua
+++ scenes/game.lua
@@ -3,7 +3,7 @@ local scene = composer.newScene()
 --=================================
 local board
 local listBox = {}
-local array = 10
+local array = 20
 local countBox = 0
 local centerX = display.contentCenterX
 local centerY = display.contentCentery
@@ -18,6 +18,7 @@ local elements = require "classes.element"
 
 --=================================
 local background
+local bag
 local backGroup = display.newGroup()
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
@@ -34,6 +35,9 @@ function scene:create( event )
     elements = elements.new().obj
 	  background = display.newImageRect( backGroup, "images/BGgame.jpg", 480, 320 )
 	  background.x, background.y = 240, 160
+    bag = display.newRoundedRect( uiGroup, 240, 235, 480, 120, 10 )
+    bag:setFillColor(0.071, 0.16, 0.115)
+    bag.alpha = .5
     sceneGroup:insert( backGroup )
 end
 --=================================
@@ -49,18 +53,18 @@ function scene:show( event )
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
         --=================================
-        local x , y = 50, 170
+        local x , y = 40, 210
       for i = 1, array , 1 do
-        listBox[i] = display.newImageRect(uiGroup,"images/board.png", 25 , 25)
+        listBox[i] = display.newImageRect(uiGroup,"images/tile.png", 35 , 35)
         listBox[i].x = x
         listBox[i].y = y
         countBox = countBox + 1
         if( countBox > 9 ) then
-          y = y + 35
+          y = y + 45
           countBox = 0
-          x = 0
+          x = - 5
         end
-        x = x + 35
+        x = x + 45
       end
 
 
