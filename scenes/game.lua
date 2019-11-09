76995108aed109f60d09190695c6cddf5fa8cf0f
hqkhanh98 <tsuuuuuu66@gmail.com>
2019-11-09 10:08:31 +0700

    upload

--- scenes/game.lua
+++ scenes/game.lua
@@ -7,7 +7,7 @@ local scene = composer.newScene()
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
-
+local background
 
 
 
